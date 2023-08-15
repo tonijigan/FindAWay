43e5e34ce29787b4445903bb95f389263f2d7fc5
tonijigan <100856397+tonijigan@users.noreply.github.com>
2023-08-15 16:00:53 +0300

    init

--- Assets/Scripts/Player/PlayerMovement.cs
+++ Assets/Scripts/Player/PlayerMovement.cs
@@ -1,18 +1,42 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
+[RequireComponent(typeof(Rigidbody))]
 public class PlayerMovement : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    [SerializeField] private float _speed;
+    [SerializeField] private float _rotateSpeed;
+
+    private Rigidbody _rigidbody;
+
+    private void Start()
     {
-        
+        _rigidbody = GetComponent<Rigidbody>();
     }
 
-    // Update is called once per frame
-    void Update()
+    private void FixedUpdate()
     {
-        
+        var newDirection = GetDirection();
+        Move(newDirection);
+        MoveRotate(newDirection);
+    }
+
+    private void Move(Vector3 direction)
+    {
+        _rigidbody.MovePosition(_rigidbody.position + _speed * Time.deltaTime * direction);
+    }
+
+    private Vector3 GetDirection()
+    {
+        return new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
+    }
+
+    private void MoveRotate(Vector3 direction)
+    {
+        if (direction != Vector3.zero)
+        {
+            transform.rotation = Quaternion.Lerp(transform.rotation,
+                                 Quaternion.LookRotation(direction),
+                                 _rotateSpeed * Time.deltaTime);
+        }
     }
 }
