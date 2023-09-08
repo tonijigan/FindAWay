a941d9d1db271a052df1e13551c07d94bd0157ff
tonijigan <100856397+tonijigan@users.noreply.github.com>
2023-09-08 09:07:22 +0300

    Исправление скрипта у джостика

--- Assets/Scripts/Joystick/Joystick.cs
+++ Assets/Scripts/Joystick/Joystick.cs
@@ -1,7 +1,8 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 using UnityEngine.EventSystems;
+using UnityEngine.XR;
+
+public enum AxisOptions { Both, Horizontal, Vertical }
 
 public class Joystick : MonoBehaviour, IPointerDownHandler, IDragHandler, IPointerUpHandler
 {
@@ -46,20 +47,13 @@ public class Joystick : MonoBehaviour, IPointerDownHandler, IDragHandler, IPoint
         DeadZone = deadZone;
         baseRect = GetComponent<RectTransform>();
         canvas = GetComponentInParent<Canvas>();
-        if (canvas == null)
-            Debug.LogError("The Joystick is not placed inside a canvas");
-
-        Vector2 center = new Vector2(0.5f, 0.5f);
+        float centerObject = 0.5f;
+        Vector2 center = new Vector2(centerObject, centerObject);
         background.pivot = center;
         handle.anchorMin = center;
         handle.anchorMax = center;
         handle.pivot = center;
         handle.anchoredPosition = Vector2.zero;
-    }
-
-    public virtual void OnPointerDown(PointerEventData eventData)
-    {
-        OnDrag(eventData);
     }
 
     public void OnDrag(PointerEventData eventData)
@@ -68,65 +62,18 @@ public class Joystick : MonoBehaviour, IPointerDownHandler, IDragHandler, IPoint
         if (canvas.renderMode == RenderMode.ScreenSpaceCamera)
             cam = canvas.worldCamera;
 
+        int divider = 2;
         Vector2 position = RectTransformUtility.WorldToScreenPoint(cam, background.position);
-        Vector2 radius = background.sizeDelta / 2;
+        Vector2 radius = background.sizeDelta / divider;
         input = (eventData.position - position) / (radius * canvas.scaleFactor);
         FormatInput();
         HandleInput(input.magnitude, input.normalized, radius, cam);
         handle.anchoredPosition = input * radius * handleRange;
     }
 
-    protected virtual void HandleInput(float magnitude, Vector2 normalised, Vector2 radius, Camera cam)
+    public virtual void OnPointerDown(PointerEventData eventData)
     {
-        if (magnitude > deadZone)
-        {
-            if (magnitude > 1)
-                input = normalised;
-        }
-        else
-            input = Vector2.zero;
-    }
-
-    private void FormatInput()
-    {
-        if (axisOptions == AxisOptions.Horizontal)
-            input = new Vector2(input.x, 0f);
-        else if (axisOptions == AxisOptions.Vertical)
-            input = new Vector2(0f, input.y);
-    }
-
-    private float SnapFloat(float value, AxisOptions snapAxis)
-    {
-        if (value == 0)
-            return value;
-
-        if (axisOptions == AxisOptions.Both)
-        {
-            float angle = Vector2.Angle(input, Vector2.up);
-            if (snapAxis == AxisOptions.Horizontal)
-            {
-                if (angle < 22.5f || angle > 157.5f)
-                    return 0;
-                else
-                    return (value > 0) ? 1 : -1;
-            }
-            else if (snapAxis == AxisOptions.Vertical)
-            {
-                if (angle > 67.5f && angle < 112.5f)
-                    return 0;
-                else
-                    return (value > 0) ? 1 : -1;
-            }
-            return value;
-        }
-        else
-        {
-            if (value > 0)
-                return 1;
-            if (value < 0)
-                return -1;
-        }
-        return 0;
+        OnDrag(eventData);
     }
 
     public virtual void OnPointerUp(PointerEventData eventData)
@@ -145,6 +92,68 @@ public class Joystick : MonoBehaviour, IPointerDownHandler, IDragHandler, IPoint
         }
         return Vector2.zero;
     }
-}
 
-public enum AxisOptions { Both, Horizontal, Vertical }
+    protected virtual void HandleInput(float magnitude, Vector2 normalised, Vector2 radius, Camera cam)
+    {
+        int acceptableZone = 1;
+
+        if (magnitude > deadZone)
+        {
+            if (magnitude > acceptableZone)
+                input = normalised;
+        }
+        else
+            input = Vector2.zero;
+    }
+
+    private void FormatInput()
+    {
+        float positionY = 0;
+
+        if (axisOptions == AxisOptions.Horizontal)
+            input = new Vector2(input.x, positionY);
+        else if (axisOptions == AxisOptions.Vertical)
+            input = new Vector2(0f, input.y);
+    }
+
+    private float SnapFloat(float value, AxisOptions snapAxis)
+    {
+        int minValue = 0;
+        int maxValue = 1;
+        float minAcceptableHorizontalAngle = 22.5f;
+        float minAcceptableVerticalAngle = 67.5f;
+        float maxAcceptableHorizontalAngle = 157.5f;
+        float maxAcceptableVerticalAngle = 112.5f;
+
+        if (value == minValue)
+            return value;
+
+        if (axisOptions == AxisOptions.Both)
+        {
+            float angle = Vector2.Angle(input, Vector2.up);
+            if (snapAxis == AxisOptions.Horizontal)
+            {
+                if (angle < minAcceptableHorizontalAngle || angle > maxAcceptableHorizontalAngle)
+                    return minValue;
+                else
+                    return (value > minValue) ? maxValue : -maxValue;
+            }
+            else if (snapAxis == AxisOptions.Vertical)
+            {
+                if (angle > minAcceptableVerticalAngle && angle < maxAcceptableVerticalAngle)
+                    return minValue;
+                else
+                    return (value > minValue) ? maxValue : -maxValue;
+            }
+            return value;
+        }
+        else
+        {
+            if (value > minValue)
+                return maxValue;
+            if (value < minValue)
+                return -maxValue;
+        }
+        return minValue;
+    }
+}
